7c52a8ff4c5ae1f6561fb84223f0eb5e5ac0b86a
msabeni <kemimsabeni@gmail.com>
2022-06-19 15:23:38 +0300

    working on django models

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=1500)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    created_date = models.DateTimeField('date published')
+    published_date = models.DateTimeField('date published')
